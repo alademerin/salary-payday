aedb4b7ac8cee32f77d966fc129b4a472e00ca08
alademerin <davidalademerin@gmail.com>
2023-06-19 20:47:31 +0100

    refactored navbar component to use loops to dynamically render links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,13 @@ import Slogan from "../assets/slogan.svg";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navItems = [
+    { name: "Home", to: "/" },
+    { name: "Solutions", to: "/solutions" },
+    { name: "About", to: "/about" },
+    { name: "Contact", to: "/contact" },
+  ];
+
   return (
     <nav className="flex justify-between items-center pt-10 px-6  lg:px-36 lg:pt-6">
       <div>
@@ -11,42 +18,16 @@ const Navbar = () => {
         <img src={Slogan} />
       </div>
       <div className="hidden  space-x-10 items-center lg:flex">
-        <NavLink
-          className={({ isActive, isPending }) =>
-            isPending ? "text-grey" : isActive ? "text-primaryOrange" : "text-grey"
-          }
-          to="/"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive, isPending }) =>
-            isPending ? "text-grey" : isActive ? "text-primaryOrange" : "text-grey hover:text-primaryOrange"
-          }
-          to="/error"
-        >
-          Solutions
-        </NavLink>
-        <NavLink
-          className={({ isActive, isPending }) =>
-            isPending
-              ? "text-grey"
-              : isActive
-              ? "text-primaryOrange"
-              : "text-grey hover:text-primaryOrange"
-          }
-          to="/error"
-        >
-          About
-        </NavLink>
-        <NavLink
-          className={({ isActive, isPending }) =>
-            isPending ? "text-grey" : isActive ? "text-primaryOrange" : "text-grey hover:text-primaryOrange"
-          }
-          to="/error"
-        >
-          Contact
-        </NavLink>
+        {navItems.map((navItem, idx) => (
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending ? "text-grey" : isActive ? "text-primaryOrange" : "text-grey"
+            }
+            to={navItem.to}
+          >
+            {navItem.name}
+          </NavLink>
+        ))}
         <Link
           to="/login"
           className="bg-primaryOrange hover:opacity-90 px-16 py-4 rounded-full text-white"
